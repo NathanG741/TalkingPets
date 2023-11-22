0d5758f59c766b29e05805e1d93749afaef20c61
nathan <nathan.guardado@yahoo.com>
2023-11-22 12:43:32 -0500

    Finished

--- src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
+++ src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
@@ -5,4 +5,16 @@ import org.junit.Test;
 public class MainApplicationTest {
 @Test
     public void testMe(){}
+
+    public void testGetHowManyPets() {
+    }
+
+    public void testGetWhatKindOfPet() {
+    }
+
+    public void testGetNameOfPet() {
+    }
+
+    public void testMain() {
+    }
 }
